be74ae73d80bc3a134441c97816fef2169ad7d78
Paul Lancaster <paul@lancasterzone.com>
2020-06-09 23:51:43 +0100

    Playing around with training model

--- database/select_aircraft_images.sql
+++ database/select_aircraft_images.sql
@@ -2,4 +2,7 @@ SELECT aircraft_images.path, aircraft_variants.variantname
 FROM aircraft_images 
 INNER JOIN aircraft_variants 
 ON aircraft_images.variantid = aircraft_variants.variantid
+AND (aircraft_variants.variantname = '737-800'
+OR aircraft_variants.variantname = 'c-130'
+OR aircraft_variants.variantname = 'cessna 172')
 ORDER BY random()
