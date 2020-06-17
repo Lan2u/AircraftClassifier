d3e87decddb3b7e6304217f759093d427e899992
Paul Lancaster <paul@lancasterzone.com>
2020-06-17 23:24:28 +0100

    Tried a few more things

--- database/select_aircraft_images.sql
+++ database/select_aircraft_images.sql
@@ -1,8 +1,5 @@
-SELECT aircraft_images.path, aircraft_variants.variantname 
+SELECT aircraft_images.path, aircraft_variants.variantid
 FROM aircraft_images 
 INNER JOIN aircraft_variants 
 ON aircraft_images.variantid = aircraft_variants.variantid
-AND (aircraft_variants.variantname = '737-800'
-OR aircraft_variants.variantname = 'c-130'
-OR aircraft_variants.variantname = 'cessna 172')
 ORDER BY random()
