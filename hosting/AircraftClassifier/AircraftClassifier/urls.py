6654754d23f2033d795215adf0038ac7ff8b1c0e
Paul Lancaster <paul@lancasterzone.com>
2020-07-18 23:11:35 +0100

    Getting read to start on predict_image view

--- hosting/AircraftClassifier/AircraftClassifier/urls.py
+++ hosting/AircraftClassifier/AircraftClassifier/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from aircraft import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('predict/', views.predict_image, name='predict')
 ]
