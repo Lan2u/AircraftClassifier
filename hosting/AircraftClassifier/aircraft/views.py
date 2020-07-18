d3d1aa1699176a69357b479f52716cea5d9c16a3
Paul Lancaster <paul@lancasterzone.com>
2020-07-18 23:53:43 +0100

    Setup predict page requirements (bootstrap, jquery, css, js)

--- hosting/AircraftClassifier/aircraft/views.py
+++ hosting/AircraftClassifier/aircraft/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 # Create your views here.
 def predict_image(request):
-    return HttpResponse("Predict Image View")
+    template = loader.get_template('aircraft/predict.html')
+    context = {}
+    return render(request, 'aircraft/predict.html', context)
