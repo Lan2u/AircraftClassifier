8f4de882a53025bc71551df5db8634993d6d7fdf
Paul Lancaster <paul@lancasterzone.com>
2020-07-20 14:47:56 +0100

    Pillow image loading working

--- hosting/AircraftClassifier/aircraft/classifier/VariantClassifier.py
+++ hosting/AircraftClassifier/aircraft/classifier/VariantClassifier.py
@@ -1,4 +1,23 @@
-
+import os
+from tensorflow import keras
+import PIL
+import io
 
 
 class VariantClassifier:
+    def __init__(self):
+        model_path = os.path.join(os.getcwd(), 'aircraft', 'classifier', 'trained_model')
+        self.model = keras.models.load_model(model_path)
+
+    def load_image(self, bytes):
+        # image = PIL.Image.frombytes()
+        data = io.BytesIO(bytes)
+        return PIL.Image.open(data)
+        # image.show()
+
+    def preprocess(self, img):
+        return img
+
+    def predict(self, img):
+        image = self.load_image(img)
+        return self.model.predict(self.preprocess(image))
--- hosting/AircraftClassifier/aircraft/views/PredictAircraftVariant.py
+++ hosting/AircraftClassifier/aircraft/views/PredictAircraftVariant.py
@@ -3,15 +3,26 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+from aircraft.classifier.VariantClassifier import VariantClassifier
+
+
 class PredictAircraftVariant(TemplateView):
     ENDPOINT = 'predict/aircraft/variant/'
 
     template_name = 'aircraft/api.html'
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.classifier = VariantClassifier()
+
+
     @csrf_exempt
     def post(self, request):
         print("Predict aircraft variant called")
-        with open('temp.jpg', 'wb') as temp:
-            for chunk in request.FILES['predictFile'].chunks():
-                temp.write(chunk)
+        img = request.FILES['predictFile'].read()
+        self.classifier.predict(img)
+
+        # with open('temp.jpg', 'wb') as temp:
+        #     for chunk in request.FILES['predictFile'].chunks():
+        #         temp.write(chunk)
         return HttpResponse(status=501, reason="Predict API not implemented")
