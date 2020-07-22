4b45268e52b20e8273c7d8e54a9981fe59191d7c
Paul Lancaster <paul@lancasterzone.com>
2020-07-22 12:20:24 +0100

    Added test to check prediction on 2 images

--- hosting/AircraftClassifier/aircraft/tests.py
+++ hosting/AircraftClassifier/aircraft/tests.py
@@ -7,6 +7,7 @@ import io
 MODEL_PATH = path = os.path.join(os.getcwd(), 'classifier', 'trained_model')
 
 TEST_IMG_PATH = 'test-image.jpg'
+TEST_IMG_PATH_2 = 'test-image-2.jpg'
 
 
 # Create your tests here.
@@ -19,3 +20,17 @@ def test_predict_variant_classifier():
     with open(TEST_IMG_PATH, "rb") as f:
         result = classifier.predict(f.read())
         assert len(result) == VariantClassifier.class_count()
+
+
+# Tests prediction on different images, checks that the images return different results.
+def test_predict_diff_images():
+    classifier = VariantClassifier(MODEL_PATH)
+    with open(TEST_IMG_PATH, "rb") as f:
+        result = classifier.predict(f.read())
+        assert len(result) == VariantClassifier.class_count()
+
+        with open(TEST_IMG_PATH_2, "rb") as f2:
+            result2 = classifier.predict(f2.read())
+            assert len(result2) == VariantClassifier.class_count()
+            assert not (result is result2)
+            assert result != result2
